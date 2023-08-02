d8b392f50bd468a45e24116e7e1d5fee7d905ab6
SruthiAmmu2 <110349740+SruthiAmmu2@users.noreply.github.com>
2023-08-02 22:10:47 +0100

    Problem Statement 1

---
+++ ProblemStatement1/src/com/ProblemStatement2/OverrideCalculation/OverloadedMethod.java
@@ -0,0 +1,14 @@
+package com.ProblemStatement2.OverrideCalculation;
+
+public class OverloadedMethod {
+
+	
+	public static void main(String[] args) {
+		
+		OverrideSum obj = new OverrideSum();
+		obj.multiply(2,3);
+		obj.multiply(20.5f,20.5f);	
+	}
+
+}
+
